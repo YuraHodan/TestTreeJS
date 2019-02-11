3a818061d3d8e0d674370d3afd4001d89d00d775
yurahodan <yura.hodan@gmail.com>
2019-02-11 02:04:59 +0200

    corrected counting errors id

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let tree =[];
-var elementID;
-var parent;
+let elementID;
+let parent;
 window.TreeAPI.getData().then(function(defs){
   tree = defs.data
   function createList(spacecrafts){
-  var listView=document.createElement('ul');
-  for(var i=0;i<spacecrafts.length;i++){
+  let listView=document.createElement('ul');
+  for(let i=0;i<spacecrafts.length;i++){
       elementID = tree[i].id;
       parent = tree[i].parent;
       if(spacecrafts[i].parent == null){
         document.getElementById('tree').innerHTML =
         '<ul><li><a>'+ spacecrafts[i].id +'</a><div class="actions"><button class="add" onclick="addIt(this.value)" value="'+elementID+'">add</button></div><ul id ="'+ spacecrafts[i].id +'"></ul>'
       }else{
-        var newLi = document.createElement('li');
+        let newLi = document.createElement('li');
         newLi.id = elementID
         newLi.innerHTML =
         '<a>'+ elementID +'</a><div class="actions"><button class="add" onclick="addIt(this.value)" value="'+elementID+'">add</button><button onclick="deleteIt(this.value)"  class="remove" value="'+ elementID +'">remove</button ></div><ul class="vertical" id ="'+ elementID +'"></ul>'
@@ -24,17 +24,33 @@ window.TreeAPI.getData().then(function(defs){
   document.getElementById("tree").appendChild(createList(tree));
 });
 
-// add new item
+// delete item
 function deleteIt(deleteItem){
   tree=tree.filter(item=>item.id!=deleteItem);
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].parent == deleteItem) {
+      tree=tree.filter(item=>item.parent!=tree[i].id);
+    };
+  };
+  tree=tree.filter(item=>item.parent!=deleteItem);
   document.getElementById(deleteItem).outerHTML = "";
   };
-// delete item
+// add new item
   function addIt(addIt){
-    let newId = +tree[tree.length-1].id+1;
+    let newId;
+    let result = tree.map(a => Number(a.id));
+    let count = result[result.length - 1];
+        for ( let i = 1; i <= count; i++ ) {
+          if (result.indexOf(i) == -1) {
+            newId=i;
+            break;
+          }else {
+             newId = +tree[tree.length-1].id+1;
+          };
+      };
     tree.push({id:newId,parent:addIt})
     //I do not see the content to convert the number into a string (id)
-    var newItem = document.createElement('li');
+    let newItem = document.createElement('li');
     newItem.id = newId
     newItem.innerHTML =
     '<a>'+ newId +'</a><div class="actions"><button class="add" onclick="addIt(this.value)"  value="'+newId+'">add</button><button onclick="deleteIt(this.value)"  class="remove" value="'+ newId +'">remove</button ></div><ul class="vertical" id ="'+ newId +'"></ul>'
